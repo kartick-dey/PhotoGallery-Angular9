ec926474d16d94b18b4650ce04dd576aa57dc3b3
Kartick Dey <kartick.dey1995@gmail.com>
2020-10-19 12:10:53 +0530

    Adding header and home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+
+    const content = document.querySelector('.container-fuild');
+    const scroll$ = fromEvent(content, 'scroll').pipe(
+      map(() => content)
+    );
+
+    scroll$.subscribe(element => {
+      console.log('Element: ', element);
+    });
   }
 
 }
